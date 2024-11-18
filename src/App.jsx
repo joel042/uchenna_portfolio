335fca08b41815488223d8e42bf88e9e2a75cfd7
joel042 <uchennajoeleze@gmail.com>
2024-11-18 12:34:03 +0100

    '.'

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
-import Home from './pages/Home'
+import Home from './pages/Home';
+import About from './pages/About';
+import { Routes, Route } from 'react-router-dom';
+// import Card from './components/Card';
 
 function App() {
   return (
-    <div className=''>
-
-      <Home />
+    <div className="">
+    {/* <Card /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
 export default App
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import ucheImg from "../assets/uchenna id.jpg"
+import React from "react";
+import ucheImg from "../assets/uchenna id.jpg";
+import { NavLink } from "react-router-dom";
 
 function Card() {
   return (
@@ -22,26 +23,45 @@ function Card() {
           </div>
         </div>
         <div className="grid grid-cols-2 gap-2">
-          <button className=" h-22 text-gray-100 border border-gray-100 flex items-center justify-center">
-            ABOUT ME
-          </button>
+          <NavLink
+            to="/about"
+            className=" h-22 text-gray-100 border border-gray-100 flex items-center justify-center"
+          >
+            <button>ABOUT ME</button>
+          </NavLink>
+
           <button className=" h-[50px] text-gray-100 border border-gray-100 flex items-center justify-center">
             PORTFOLIO
           </button>
         </div>
         <div className="grid grid-cols-1 gap-1 ">
-          <button className="bg-red-500 h-[50px] text-white flex items-center justify-center">
-            HIRE ME
-          </button>
-          <button className="bg-red-500 h-[50px] text-white flex items-center justify-center">
-            HIRE ME
-          </button>
-          
-        </div>
+          {/* <button className="bg-red-700 button h-[50px] text-white flex items-center justify-center">
+           DOWNLOAD CV
+          </button> */}
 
+          <button className="button" type="button">
+            <span className="button__text">DOWNLOAD CV</span>
+            <span className="button__icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 35 35"
+                id="bdd05811-e15d-428c-bb53-8661459f9307"
+                data-name="Layer 2"
+                className="svg"
+              >
+                <path d="M17.5,22.131a1.249,1.249,0,0,1-1.25-1.25V2.187a1.25,1.25,0,0,1,2.5,0V20.881A1.25,1.25,0,0,1,17.5,22.131Z"></path>
+                <path d="M17.5,22.693a3.189,3.189,0,0,1-2.262-.936L8.487,15.006a1.249,1.249,0,0,1,1.767-1.767l6.751,6.751a.7.7,0,0,0,.99,0l6.751-6.751a1.25,1.25,0,0,1,1.768,1.767l-6.752,6.751A3.191,3.191,0,0,1,17.5,22.693Z"></path>
+                <path d="M31.436,34.063H3.564A3.318,3.318,0,0,1,.25,30.749V22.011a1.25,1.25,0,0,1,2.5,0v8.738a.815.815,0,0,0,.814.814H31.436a.815.815,0,0,0,.814-.814V22.011a1.25,1.25,0,1,1,2.5,0v8.738A3.318,3.318,0,0,1,31.436,34.063Z"></path>
+              </svg>
+            </span>
+          </button>
+          <button className="bg-red-700 font-semibold h-[50px] text-white flex items-center justify-center">
+            HIRE ME
+          </button>
+        </div>
       </div>
     </div>
   );
 }
 
-export default Card
+export default Card;
